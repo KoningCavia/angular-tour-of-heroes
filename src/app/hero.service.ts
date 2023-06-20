9a6417b5cd4dc92ef0b0f036cf8160bbe4d4ac1e
Wicher <wicherICT@hotmail.com>
2023-06-20 18:32:57 +0200

    heroes are now retrieved through the service layer. Methods are adapted to deal with asynchronous calls

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Hero} from "./hero";
 import { HEROES } from "./mock-heroes";
+import {Observable, of} from "rxjs";
 
 @Injectable({
   // 1  injectables can be made available in several methods. in this case in the service itself by providing the line "providedIn: 'root' ".
@@ -11,8 +12,14 @@ import { HEROES } from "./mock-heroes";
 // 2 a service can get its (hero) information from anywhere. web storage, local or a mock data source.
 export class HeroService {
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  // getHeroes(): Hero[] {
+  //   return HEROES;
+  // }
+
+  getHeroes(): Observable<Hero[]> {
+    //of(HEROES) returns a single value. an array of mock heroes
+    const heroes = of(HEROES);
+    return heroes;
   }
 
   constructor() { }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,7 +2,7 @@
 import { Component } from '@angular/core';
 import {Hero} from "../hero";
 //Importing the simple HEROES list made somewhere else.
-import {HEROES} from "../mock-heroes";
+import { HeroService } from "../hero.service";
 
 
 @Component({
@@ -15,11 +15,25 @@ import {HEROES} from "../mock-heroes";
 // allways export the component class, so it can be imported elsewhere
 export class HeroesComponent {
   //de heroesComponent class is dus niet de hero class met diens variabelen. die staat in de hero.ts interface beschreven. een Component is een frontend view, een interface is functioneel een klasse bescrhijving
+  //that being said. dit zijjn wel de variabelen die deze view nodig heeft "nameVariable?: nameClass".
   selectedHero?: Hero;
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) {}
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  heroes = HEROES;
+// this method uses the subscribe part to deal with asynchronous calls which return an observable object, rather than just an object.
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  //basically on init, call the method getHeroes
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
 }
