b09e8a403a7b43512faf8522fbc5292fa14508e7
Wicher <wicherICT@hotmail.com>
2023-07-25 14:43:44 +0200

    added delete from db functionality and searchfor hero by name functionality

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -50,11 +50,36 @@ export class HeroService {
     );
   }
 
+  /** post: save a new hero on the server*/
   addHero(hero: Hero): Observable<Hero> {
     return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
       tap((newHero:Hero) => this.log(`added hero w/ id/${newHero.id}`)),
       catchError(this.handleError<Hero>('AddHero'))
     )
+  }
+
+  /** delete: remove a hero from the server */
+
+  deleteHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=$`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
+  /** get heroes whos name contains search term */
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?ma,e=${term}`).pipe(
+      tap(x => x.length ?
+      this.log(`found heroes matching "${term}"`) :
+        this.log(`no heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
   }
 
 
